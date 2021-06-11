c269d8aeafc2154b4bf7fd06d4b42be6f79406fd
diego-mesiano <diego.mesiano1@gmail.com>
2021-06-11 13:49:47 -0300

    Quase reslvido

--- progImperativa/d10-06-21-js/arrays.js
+++ progImperativa/d10-06-21-js/arrays.js
@@ -89,13 +89,53 @@ utilizando  .toUpperCase() para cada elemento.*/
 //console.log('TUDO EM MAISCULO:',filme);
 console.log('\nTudo normal:',filme);
 
-console.log('Tudo maiusculo:',arrayMaiuscula(filme));
+
 
 function arrayMaiuscula(InserirArray){ //Cria Função que transforma uma array em maiuscula
-    let permanente = ['a','b']; //receberá a array transformada
+    let permanente = []; //receberá a array transformada
     for (let j = 0; j < InserirArray.length; j++){//varre a array de acordo com o seu tamanho através do .lenght
         permanente[j] = InserirArray[j];//transfere os valores originais para a nova variavel
         permanente[j] = permanente[j].toUpperCase();}//converte o texto da array em maiusculo através do .toUpperCase
     return permanente;}//retorna a array transformada, é necessario atraibuir a uma variavel array
+filme=arrayMaiuscula(filme); //deve se atribuir a ele mesmo ou outra array para aplicar as mudanças
+console.log('Tudo maiusculo:',filme);
+
+//3)Enquanto trabalhava no exercício 2, descobrimos que também há outro array no código, mas com filmes animados. 
+//let cartoons = ["toy story", "Procurando Nemo", "kung-fu panda", "wally", "fortnite"]
+//4)No mesmo arquivo, crie este array e uma função que adiciona cada elemento do array de desenhos animados (cartoons) ao array de filmes.
+
+let cartoons = ["toy story", "Procurando Nemo", "kung-fu panda", "wally", "fortnite"];
+let juncao = filme.concat(cartoons);
+filme=juncao;
+console.log('\nArrays concatenadas com sucesso!',filme);
+
+//5)Um dos desenvolvedores avisou que a última animação é na verdade um jogo, então remova esse último elemento do array de desenhos 
+//animados (cartoons) antes de passá-los para os filmes.
+
+filme.pop();
+console.log('\nRemovido ultimo elemento "fortinite" com sucesso!',filme);
+
+
+/*6)NÃO FAZER POR ENQUANTO: Por fim, eles nos enviam dois arrays com as avaliações feitas por diferentes usuários do mundo desses filmes, 
+da seguinte forma:
+const asiaScores = [8, 10, 6, 9, 10, 6, 6, 8];
+const euroScores = [8, 10, 6, 8, 10, 6, 7, 9]; 
+
+Eles nos pedem para criar uma função que compare as notas e nos diga se elas são iguais ou diferentes. Eles confirmam que estão na 
+respectiva ordem e que trazem apenas valores numéricos de 1 a 10. Para verificar se tudo está bem até agora, recomendamos testar cada 
+uma das funções e testar seu funcionamento correto.*/
+
+const asiaScores = [8, 10, 6, 9, 10, 6, 6, 8];
+const euroScores = [8, 10, 6, 8, 10, 6, 7, 9]; 
+
+for(let i=0; i<asiaScores.length; i++){
+    console.log('Trabalhando no indice',i,'\nComparando',asiaScores[i],'com',euroScores[i],'......');
+    if (asiaScores[i] == euroScores[i]){
+        console.log('Igual!\n');
+    }else{
+        console.log('Diferente!!\n');
+    }
+}
+
 
 

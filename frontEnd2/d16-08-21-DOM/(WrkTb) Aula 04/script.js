9e3e35e31150372dac07cdc7979095db753d5a7f
diego-mesiano <diego.mesiano1@gmail.com>
2021-08-17 08:48:10 -0300

    Conteúdo de DOM js

---
+++ frontEnd2/d16-08-21-DOM/(WrkTb) Aula 04/script.js
@@ -0,0 +1,9 @@
+let bodi = document.querySelector("body");
+let agaum = document.querySelector("header");
+let conteudu = document.querySelector(".conteudo");
+let sequition = document.querySelector("section");
+
+console.log(bodi);
+console.log(agaum);
+console.log(conteudu);
+console.log(sequition);

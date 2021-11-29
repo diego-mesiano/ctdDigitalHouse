817de7574a5a89d4ff730a65503595cc1fd8edde
diego-mesiano <diego.mesiano1@gmail.com>
2021-11-29 12:18:04 -0300

    Aula sobre Jenkins

---
+++ infra2/jenkinst-tquatro/src/main/java/com/diego/jenkinsttquatro/JenkinstTquatroApplication.java
@@ -0,0 +1,13 @@
+package com.diego.jenkinsttquatro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinstTquatroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinstTquatroApplication.class, args);
+	}
+
+}
---
+++ infra2/jenkinst-tquatro/src/test/java/com/diego/jenkinsttquatro/JenkinstTquatroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.jenkinsttquatro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinstTquatroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
